32bb644e00cfcb838e22edff827e0ee2ab9af427
Kirill Konotop <154094499+kirillcocotope@users.noreply.github.com>
2023-12-17 23:13:15 +0200

    Create limiter.js

---
+++ looses/limiter.js
@@ -0,0 +1,18 @@
+import '@testing-library/jest-dom/extend-expect'
+import { setConfig } from 'next/config'
+import config from './next.config'
+
+    "@testing-library/react": "^13.2.0",
+    "@testing-library/user-event": "14.2.0",
+    "@types/react": "18.0.9",
+
+
+              "denom": "ubedrock",
+        "displayName": "UBEDROCK",
+        "fraction": 1000000
+      },
+      {
+        "denom": "umuon",
+        "displayName": "MUON",
+        "fraction": 1000000
+      },
